b967ce29ef7b20d4d9a6e07d493a1d2724f4be5e
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-06-20 16:34:52 -0700

    Waterflow generalizing

--- waterflow_test/waterflow_test.ino
+++ waterflow_test/waterflow_test.ino
@@ -1,5 +1,4 @@
 #include <math.h>
-long time = millis();
 
 #define LOW_PRESSURE_1 A0
 #define LOW_PRESSURE_2 A1
@@ -12,6 +11,8 @@ long time = millis();
 int input = 0;
 bool shouldPrint = false;
 
+long currTime = millis();
+
 int numLowPressure = 0;
 int numHighPressure = 0;
 
@@ -21,78 +22,173 @@ void setup() {
   Serial.println("How many low pressure sensors are connected?");
   while (!Serial.available());
   
-  int lowPressureRead = Serial.read();
-  numLowPressure = lowPressureRead - 48;
+  numLowPressure = Serial.read() - 48;
   
-  Serial.println("How many low pressure sensors are connected?");
+  Serial.println("How many high pressure sensors are connected?");
   while (!Serial.available());
 
   numHighPressure = Serial.read() - 48;
 
-  pinMode(LOW_PRESSURE_1, INPUT);
-  pinMode(LOW_PRESSURE_2, INPUT);
-  pinMode(LOW_PRESSURE_3, INPUT);
-  pinMode(LOW_PRESSURE_4, INPUT);
-  
-  pinMode(HIGH_PRESSURE_1, INPUT);
-  pinMode(HIGH_PRESSURE_2, INPUT);
+  if(numLowPressure >= 1){
+    pinMode(LOW_PRESSURE_1, INPUT);
+  }
+  if(numLowPressure >= 2){
+    pinMode(LOW_PRESSURE_2, INPUT);
+  }
+  if(numLowPressure >= 3){
+    pinMode(LOW_PRESSURE_3, INPUT);
+  }
+  if(numLowPressure >= 4){
+    pinMode(LOW_PRESSURE_4, INPUT);
+  }
 
+  if(numHighPressure >= 1){
+    pinMode(HIGH_PRESSURE_1, INPUT);
+  }
+  if(numHighPressure >= 2){
+    pinMode(HIGH_PRESSURE_2, INPUT);
+  }
+
+  Serial.print("There are ");
+  Serial.print(numLowPressure);
+  Serial.print(" low PTs and ");
+  Serial.print(numHighPressure);
+  Serial.println(" high PTs");
 }
 
 // 0.88V - 4.4V : ?? - 5000 PSI
 
-int lowpressurelox, lowpressureprop, highpressurelox, highpressureprop, lowpressureinjector, highpressure;
-int converted_lox_low, converted_prop_low, converted_lox_high, converted_prop_high, converted_inject_low, converted_high;
+int lowPressure1, lowPressure2, lowPressure3, lowPressure4, highPressure1, highPressure2;
+int convertedLow1, convertedLow2, convertedLow3, convertedLow4, convertedHigh1, convertedHigh2;
 
-int periodic = 50;
+int periodic = 100; // take data 10 times a second.
 void loop() {
-  if (Serial.available() > 0)   {
-    int readByte = Serial.read();
-    if(readByte == 't'){
-      shouldPrint = true;
-    } else if(readByte == 'f'){
-      shouldPrint = false;
-    } else if(readByte == '0'){
-      shouldPrint = !shouldPrint;
+  currTime = millis();
+  if((currTime%int(periodic)) == 0) {
+    if (Serial.available() > 0) {
+      int readByte = Serial.read();
+      if(readByte == 't'){
+        shouldPrint = true;
+      } else if(readByte == 'f'){
+        shouldPrint = false;
+      } else if(readByte == '0'){
+        shouldPrint = !shouldPrint;
+      }
     }
-  }
   
-  //lowpressurelox = analogRead(LOW_PRESSURE_LOX);
-  lowpressureprop = analogRead(LOW_PRESSURE_PROP);
-  lowpressureinjector = analogRead(LOW_PRESSURE_INJECTOR);
-  //highpressureprop = analogRead(HIGH_PRESSURE_PROP);
-  //highpressure = analogRead(HIGH_PRESSURE_PROP);
+    readData();
   
-
-  //converted_lox_low = lowPressureConversion(lowpressurelox);
-  converted_prop_low = lowPressureConversion(lowpressureprop);
-  converted_inject_low = lowPressureConversion(lowpressureinjector);
-
-  //converted_high = highPressureConversion(highpressure);
-  //converted_prop_high= highPressureConversion(highpressureprop);
-
-  char buffer[25];
- 
-  time = millis();
-  if((time%int(periodic)) == 0){
-//  sprintf(buffer, "%d,%d,%d,%d,%d", lowpressurelox, lowpressureprop, highpressurelox, highpressureprop, lowpressureinjector);
-//  sprintf(buffer, "%d,%d,%d,%d,%d", converted_lox_low, converted_prop_low, converted_high_lox, converted_high_prop, converted_inject_low);
-    //String toWriteRaw = String(lowpressurelox)+','+String(lowpressureprop)+','+String(highpressure);
-    String toWrite = String(converted_inject_low)+','+String(converted_prop_low); // +','+String(converted_prop_high); //+','+String(converted_high_prop);
-    //if(converted_lox_low > 10 || converted_prop_low > 10  || converted_high_prop > 40){
+    convertData();
+    
+    //need some check on magnitude of reading to see if we should print data.
     if(shouldPrint){
-      Serial.println(toWrite);
+      if(numLowPressure >= 1){
+        Serial.print(convertedLow1);
+        //Serial.println("Added first low PT reading; 
+      }
+      if(numLowPressure >= 2){
+        //sprintf(toWriteBuffer + bufferIndex, "%d,", convertedLow2);
+        //bufferIndex += String(convertedLow2).length();
+        Serial.print(", ");
+        Serial.print(convertedLow2);
+      }
+      if(numLowPressure >= 3){
+        //sprintf(toWriteBuffer + bufferIndex, "%d,", convertedLow3);
+        //bufferIndex += String(convertedLow3).length();
+        Serial.print(", ");
+        Serial.print(convertedLow3);
+      }
+      if(numLowPressure >= 4){
+        //sprintf(toWriteBuffer + bufferIndex, "%d,", convertedLow4);
+        //bufferIndex += String(convertedLow4).length();
+        Serial.print(", ");
+        Serial.print(convertedLow4);
+      }
+
+      if(numHighPressure >= 1){
+        //sprintf(toWriteBuffer + bufferIndex, "%d,", convertedHigh1);
+        //bufferIndex += String(convertedHigh1).length();
+        Serial.print(", ");
+        Serial.print(convertedHigh1);
+      }
+      if(numHighPressure >= 2){
+        //sprintf(toWriteBuffer + bufferIndex, "%d,", convertedHigh2);
+        //bufferIndex += String(convertedHigh2).length();
+        Serial.print(", ");
+        Serial.print(convertedHigh2);
+      }
+      Serial.print("\n");
+      
+      //String toWrite = String(converted_inject_low)+','+String(converted_prop_low); // +','+String(converted_prop_high); //+','+String(converted_high_prop);
+      //Serial.println(toWriteBuffer);
     }
-    //}
   }
 }
 
-int _lowPressureConversion(int raw){
-  return (raw - 123);
+void convertData(){
+    switch(numLowPressure){
+    case 1:
+      convertedLow1 = lowPressureConversion(lowPressure1);
+      break;
+    case 2:
+      convertedLow1 = lowPressureConversion(lowPressure1);
+      convertedLow2 = lowPressureConversion(lowPressure2);
+      break;
+    case 3:
+      convertedLow1 = lowPressureConversion(lowPressure1);
+      convertedLow2 = lowPressureConversion(lowPressure2);
+      convertedLow3 = lowPressureConversion(lowPressure3);
+      break;
+    case 4:
+      convertedLow1 = lowPressureConversion(lowPressure1);
+      convertedLow2 = lowPressureConversion(lowPressure2);
+      convertedLow3 = lowPressureConversion(lowPressure3);
+      convertedLow4 = lowPressureConversion(lowPressure4);
+      break;
+  }
+
+  switch(numHighPressure){
+    case 1:
+      convertedHigh1 = highPressureConversion(highPressure1);
+      break;
+    case 2:
+      convertedHigh1 = highPressureConversion(highPressure1);
+      convertedHigh2 = highPressureConversion(highPressure2);
+      break;
+  }
 }
 
-int _highPressureConversion(int raw){
-  return (6*raw - 1237);
+void readData(){
+  switch(numLowPressure){
+    case 1:
+      lowPressure1 = analogRead(LOW_PRESSURE_1);
+      break;
+    case 2:
+      lowPressure1 = analogRead(LOW_PRESSURE_1);
+      lowPressure2 = analogRead(LOW_PRESSURE_2);
+      break;
+    case 3:
+      lowPressure1 = analogRead(LOW_PRESSURE_1);
+      lowPressure2 = analogRead(LOW_PRESSURE_2);
+      lowPressure3 = analogRead(LOW_PRESSURE_3);
+      break;
+    case 4:
+      lowPressure1 = analogRead(LOW_PRESSURE_1);
+      lowPressure2 = analogRead(LOW_PRESSURE_2);
+      lowPressure3 = analogRead(LOW_PRESSURE_3);
+      lowPressure4 = analogRead(LOW_PRESSURE_4);
+      break;
+  }
+
+  switch(numHighPressure){
+    case 1:
+      highPressure1 = analogRead(HIGH_PRESSURE_1);
+      break;
+    case 2:
+      highPressure1 = analogRead(HIGH_PRESSURE_1);
+      highPressure2 = analogRead(HIGH_PRESSURE_2);
+      break;
+  }
 }
 
 float lowPressureConversion(int raw){
