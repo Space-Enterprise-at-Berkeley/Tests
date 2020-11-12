81139e43c5eb694c6307f817b49dbc12b666f7d9
Bruce Deng <brucedeng@berkeley.edu>
2020-11-11 18:17:22 -0800

    LoadCell Operation

--- LoadCell/LoadCellOp/LoadCellOp.ino
+++ LoadCell/LoadCellOp/LoadCellOp.ino
@@ -1,5 +1,5 @@
 /*
- Basic Load Cell Operation in Ibs
+ Basic Load Cell Operation in Kg
  Arduino pins
  5V -> VCC
  3.3V -> VDD
@@ -11,8 +11,8 @@
 
 #include "HX711.h" //This library can be obtained here http://librarymanager/All#Avia_HX711
 
-//#define calibration_factor -3950.0 // LC 1 This value is obtained using the SparkFun_HX711_Calibration sketch
-#define calibration_factor -14630 // LC2
+#define calibration_factor -3950.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
 #define LOADCELL_DOUT_PIN  3
 #define LOADCELL_SCK_PIN  2
 
@@ -20,18 +20,19 @@ HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 scale demo");
+  Serial.println("HX711 scale");
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  scale.tare();	//Assuming there is no weight on the scale at start up, reset the scale to 0
+  //scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
 
   Serial.println("Readings:");
 }
 
 void loop() {
   Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
-  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  scale.set_scale(calibration_factor);
+  Serial.print(scale.get_units() * 0.453592, 1); //scale.get_units() returns a float
+  Serial.print(" kg"); //You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
 }
