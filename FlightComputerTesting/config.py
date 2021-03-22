b0de27bc62237904f34cc411e9c0ee3c10d2452d
benjamint8 <bent8@berkeley.edu>
2021-03-21 17:41:09 -0700

    testing python influx connection

---
+++ FlightComputerTesting/config.py
@@ -0,0 +1,314 @@
+
+config_str = """
+{
+  sensors: [
+    {
+      name: "LOX Tank Pressure",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 0,
+          storageName: "loxTank",
+          label: "pressure",
+          interpolation: {
+            type: "none", // linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Prop Tank Pressure",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 1,
+          storageName: "propTank",
+          label: "pressure",
+          interpolation: {
+            type: "none", // linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "LOX Injector Pressure",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 2,
+          storageName: "loxInjector",
+          label: "pressure",
+          interpolation: {
+            type: "none", // linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Prop Injector Pressure",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 4,
+          storageName: "propInjector",
+          label: "pressure",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "LOX Dome",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 3,
+          storageName: "loxDome",
+          label: "pressure",
+          interpolation: {
+            type: "none", // linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Prop Dome",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 6,
+          storageName: "propDome",
+          label: "pressure",
+          interpolation: {
+            type: "none", // linear, quadratic
+            unit: "PSI",
+            values: [ // [x, y] pairs
+              [0, -123.89876445934394],
+              [8388607, 1131.40825] // 2^23 - 1
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Nitrogen Pressure",
+      packetId: 1,
+      values: [
+        {
+          packetPosition: 5,
+          storageName: "highPressure",
+          label: "pressure",
+          interpolation: {
+            type: "linear", // linear, quadratic
+            unit: "PSI",
+            values: [
+              [ 1634771.9270400004, 0 ],
+              [ 1771674.0096000005, 150 ],
+              [ 2544768.12288, 700 ],
+              [ 2601139.56864, 730 ],
+              [ 2681670.205440001, 805 ],
+              [ 2802466.1606400004, 890 ],
+              [ 2931315.1795200002, 990 ],
+              [ 3076270.325760001, 1100 ],
+              [ 3189013.217280001, 1200 ],
+              [ 3414499.0003200006, 1351 ],
+              [ 3543348.019200001, 1450 ],
+              [ 3704409.292800001, 1580 ],
+              [ 3946001.203200001, 1760 ],
+              [ 4155380.858880001, 1930 ],
+              [ 4380866.641920001, 2100 ],
+              [ 4509715.660800001, 2180 ],
+              [ 4751307.571200002, 2400 ],
+              [ 4976793.35424, 2550 ],
+              [ 5202279.13728, 2700 ],
+              [ 5411658.792960001, 2870 ],
+              [ 5621038.448640001, 3020 ],
+              [ 5846524.231680001, 3190 ],
+              [ 6015638.568960002, 3333 ],
+              [ 6128381.460480002, 3426 ],
+              [ 6394132.561920001, 3620 ],
+              [ 6474663.198720002, 3700 ],
+              [ 6667936.727040001, 3850 ],
+              [ 6893422.510080002, 4000 ],
+              [ 6990059.274240003, 4080 ],
+              [ 7135014.420480002, 4186 ],
+              [ 7183332.80256, 4221 ],
+              [ 7392712.4582400005, 4365 ]
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Battery",
+      packetId: 2,
+      values: [
+        {
+          packetPosition: 0,
+          storageName: "batteryVoltage",
+          label: "voltage",
+          interpolation: {
+            type: "none",
+            unit: "Volts"
+          }
+        },
+        {
+          packetPosition: 1,
+          storageName: "wattage",
+          label: "wattage",
+          interpolation: {
+            type: "none",
+            unit: "Watts"
+          }
+        },
+        {
+          packetPosition: 2,
+          storageName: "batteryAmperage",
+          label: "current",
+          interpolation: {
+            type: "none",
+            unit: "Amps"
+          }
+        }
+      ]
+    },
+    {
+      name: "LOX Aux Heater",
+      packetId: 0,
+      values: [
+        {
+          packetPosition: 1,
+          storageName: "loxAuxHeater",
+          label: "loxAuxHeater",
+          interpolation: {
+            type: "linear", // none, linear, quadratic
+            unit: "Volts",
+            values: [
+              [ 0, 0.0 ],
+              [ 255, 12.0 ]
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "LOX Gems Heater",
+      packetId: 6,
+      values: [
+        {
+          packetPosition: 1,
+          storageName: "loxGemsHeater",
+          label: "loxGemsHeater",
+          interpolation: {
+            type: "linear", // none, linear, quadratic
+            unit: "Volts",
+            values: [
+              [ 0, 0.0 ],
+              [ 255, 24.0 ]
+            ]
+          }
+        }
+      ]
+    },
+    {
+      name: "Temperatures",
+      packetId: 4,
+      values: [
+        {
+          packetPosition: 0,
+          storageName: "cryoLoxTankTemp",
+          label: "cryoLoxTankTemp",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "Celcius"
+          }
+        },
+        {
+          packetPosition: 1,
+          storageName: "cryoInj1Temp",
+          label: "cryoInj1Temp",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "Celcius"
+          }
+        },
+        {
+          packetPosition: 2,
+          storageName: "loxAdapterTreeTemp",
+          label: "loxAdapterTreeTemp",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "Celcius"
+          }
+        },
+        {
+          packetPosition: 3,
+          storageName: "loxGemsTemp",
+          label: "loxGemsTemp",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "Celcius"
+          }
+        }
+      ]
+    },
+    {
+      name: "EndflowDetection",
+      packetId: 7,
+      values: [
+        {
+          packetPosition: 0,
+          storageName: "endflowDetectionVal",
+          label: "endflowDetectionVal",
+          interpolation: {
+            type: "none", // none, linear, quadratic
+            unit: "none"
+          }
+        }
+      ]
+    }
+  ]
+}
+"""
+
+
+def gen_packet_config():
+  '''
+  Creates a dictionary in which each unique packetId has a list of the indices
+  of the associated sensors in the sensors config list
+    ex: packets[0] = [0,3,4]
+        this indicates that the sensors that are a part of packetId 0 are at
+        indices 0, 3, & 4 of config["sensors"]
+  '''
+  packets = {}
+  for i, s in enumerate(config["sensors"]):
+    if s.packetId not in packets.keys():
+      packets[s.packetId] = []
+    else:
+      packets[s.packetId].append(i)
