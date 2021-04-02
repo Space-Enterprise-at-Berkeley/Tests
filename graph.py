8d08c76a33b5b3ae78bec61569b7e2993d21712e
hannahjoo <80650373+hannahjoo@users.noreply.github.com>
2021-04-02 04:21:36 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,135 +1,127 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
-with open('smth.txt') as f:
-    line = f.readline()
-    while line:
-        line = f.readline()
-        line.split(",")
-
-lox_pt_temp = [
-    lox_tree = []
-    lox_heater = []]
-    if line[0][1] == 0:
-        lox_tree.append(line[1])
-        lox_heater.append(line[2])
-
-all_pressures = [
-    lox_tank = []
-    prop_tank = []
-    lox_inj = []
-    prop_inj = []
-    pressurant = []
-    lox_dome = []
-    prop_dome = []
-    lox_gems = []]
-    if line[0][1] == 1:
-        lox_tank.append(line[1])
-        prop_tank.append(line[2])
-        lox_inj.append(line[3])
-        prop_inj.append(line[4])
-        pressurant.append(line[5])
-        lox_dome.append(line[6])
-        prop_dome.append(line[7])
-        lox_gems.append(line[8])
-
-battery_stats = [
-    voltage = []
-    power = []
-    current_draw = []]
-    if line[0][1] == 2:
-        voltage.append(line[1])
-        power.append(line[2])
-        current_draw.append(line[3])
-
-load_cell = [
-    input1 =[]
-    input2 =[]]
-    if line[0][1] == 3:
-        input1.append(line[1])
-        input2.append(line[2])
-
-cryo_temps = [
-    cryo_lox_tank = []
-    cryo_inj_1 =[]
-    lox_adapter_tree_pt = []
-    lox_gems_ct = []
-    if line[0][1] == 4:
-        cryo_lox_tank.append(line[1])
-        cryo_inj_1.append(line[2])
-        lox_adapter_tree_pt.append(line[3])
-        lox_gems_ct.append(line[4])
+voltage = []
+power = []
+current_draw = []
+battery_stats = [voltage, power, current_draw]
 
 num_pkts_sent = []
-    if line[0][1] == 5:
-        num_pkts_sent.append(line[1])
 
-lox_gems_temp = [
-    lox_gems_temp = []
-    lox_gems_heater = []]
-    if line[0][1] == 6:
-        lox_gems_temp.append(line[1])
-        lox_gems_heater.append(line[2])
+drogue = []
+main = []
+recovery_ack = [drogue, main]
 
-endflow_detection_case = []
-    if line[0][1] == 7:
-        endflow_detection_case.append(line[1])
+lat = []
+lon = []
+gps = [lat, lon]
 
-recovery_ack = [
-    drogue = []
-    main = []]
-    if line[0][1:3] == 10:
-        drogue.append(line[1])
-        main.append(line[2])
-gps = [
-    lat = []
-    lon = []]
-    if line[0][1:3] == 11:
-        lat.append(line[1])
-        lon.append(line[2])
+fix = []
+num_sat = []
+alt = []
+speed = []
+angle = []
+gps_aux = [fix, num_sat, alt, speed, angle]
 
-gps_aux = [
-    fix = []
-    num_sat = []
-    alt = []
-    speed = []
-    angle = []]
-    if line[0][1:3] == 12:
-        fix.append(line[1])
-        num_sat.append(line[2])
-        alt.append(line[3])
-        speed.append(line[4])
-        angle.append(line[5])
+altitude = []
+pressure = []
+barometer = [altitude, pressure]
 
-barometer = [
-    altitude = []
-    pressure = []]
-    if line[0][1:3] == 13:
-        altitude.append(line[1])
-        pressure.append(line[2])
-imu_accel = [
-    accel_x = []
-    accel_y = []
-    accel_z = []]
-    if line[0][1:3] == 14:
-        accel_x.append(line[1])
-        accel_y.append(line[2])
-        accel_z.append(line[3])
+accel_x = []
+accel_y = []
+accel_z = []
+imu_accel = [accel_x, accel_y, accel_z]
 
-imu_orientation() = [
-    x = []
-    y = []
-    z = []]
-    if line[0][1:3] == 15:
-        x.append(line[1])
-        y.append(line[2])
-        z.append(line[3])
+x = []
+y = []
+z = []
+imu_orientation = [x, y, z]
 
-# x, y = np.loadtxt('smth.txt', delimiter=',', unpack=True)
-# plt.plot(x,y, label='Loaded from file!')
+with open('smth.txt') as f:
+    while line:
+        line = f.readline()[1:-1]
+        data = re.split(r",|\|")
+        id = int(line[0])
+        if id == 2:
+            voltage.append(float(data[1]))
+            power.append(float(data[2])])
+            current_draw.append(float(data[3]))
+        if id == 5:
+            num_pkts_sent.append(float(data[1]))
+        if id == 10:
+            drogue.append(float(data[1]))
+            main.append(float(data[2]))
+        if id == 11:
+            lat.append(float(data[1]))
+            lon.append(float(data[2]))
+        if id == 12:
+            fix.append(float(data[1]))
+            num_sat.append(float(data[2]))
+            alt.append(float(data[3]))
+            speed.append(float(data[4]))
+            angle.append(float(data[5]))
+        if id == 13:
+            altitude.append(float(data[1]))
+            pressure.append(float(data[2]))
+        if id == 14:
+            accel_x.append(float(data[1]))
+            accel_y.append(float(data[2]))
+            accel_z.append(float(data[3]))
+        if id == 15:
+            x.append(float(data[1]))
+            y.append(float(data[2]))
+            z.append(float(data[3]))
 
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('smth')
-# plt.legend()
-# plt.show()
+plt.plot(voltage, label = 'voltage')
+plt.plot(power, label = 'power')
+plt.plot(current_draw, label = 'current draw')
+plt.savefig("batterystats.png")
+np.save("batterystats.npy", np.array(batterystats))
+np.load("batterystats.npy")
+
+plt.plot(num_pkts_sent, label = '# packets sent')
+plt.savefig("numpktssent.png")
+np.save("numpktssent.npy", np.array(num_pkts_sent))
+np.load("numpktssent.npy")
+
+plt.plot(drogue, label = 'drogue')
+plt.plot(main, label = 'main')
+plt.savefig("recoveryack.png")
+np.save("recoveryack.npy", np.array(recovery_ack))
+np.load("recoveryack.npy")
+
+plt.plot(lat, label = 'latitude')
+plt.plot(lon, label = 'longitude')
+plt.savefig("gps.png")
+np.save("gps.npy", np.array(gps))
+np.load("gps.npy")
+
+plt.plot(fix, label = 'fix')
+plt.plot(num_sat, label = 'num sat')
+plt.plot(alt, label = 'alt')
+plt.plot(speed, label = 'speed')
+plt.plot(angle, label = 'angle')
+plt.savefig("gpsaux.png")
+np.save("gpsaux.npy", np.array(gps_aux))
+np.load("gpsaux.npy")
+
+plt.plot(altitude, label = 'altitude')
+plt.plot(pressure, label = 'pressure')
+plt.savefig("barometer.png")
+np.save("barometer.npy", np.array(barometer))
+np.load("barometer.npy")
+
+plt.plot(accel_x, label = 'accel x')
+plt.plot(accel_y, label = 'accel y')
+plt.plot(accel_z, label = 'accel z')
+plt.savefig("imuaccel.png")
+np.save("imuaccel.npy", np.array(imu_accel))
+np.load("imuaccel.npy")
+ 
+plt.plot(x, label = 'x')
+plt.plot(y, label = 'y')
+plt.plot(z, label = 'z')
+plt.savefig("imuorientation.png")
+np.save("imuorientation.npy", np.array(imu_orientation))
+np.load("imuorientation.npy")
