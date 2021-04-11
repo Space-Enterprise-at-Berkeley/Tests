cd8dc5c1116d24880618decd2eba8cb51e343cb0
Pratyush Das <42460747+das2002@users.noreply.github.com>
2021-04-11 14:25:26 -0700

    Testing for ben's first attempt
    Still need to add documentation and pictures.

---
+++ teensy_switchboard_controller/ben_button_test_1.ino
@@ -0,0 +1,78 @@
+#include <keyboard.h>
+#include <Bounce.h>
+//Pin layout here
+const int buttonPin = 1;
+Bounce pushButton = Bounce(buttonPin, 10);
+const int buttonPin2 = 4;
+const int buttonPin3 = 6;
+const int buttonPin4 = 9;
+int buttonState = 0;
+int buttonState2 = 0;
+int buttonState3 = 0;
+int buttonState4 = 0;
+
+void setup(){
+    pinMode(buttonPin, INPUT_PULLUP);
+    Serial.begin(57600);
+    pinMode(buttonPin2, INPUT_PULLUP);
+    pinMode(buttonPin3, INPUT_PULLUP);
+    pinMode(buttonPin4, INPUT_PULLUP);
+}
+void loop(){
+  buttonState = digitalRead(buttonPin);
+  byte previousState = HIGH; // what state was the button last time
+  unsigned int count =0;  // how many times has it changed to low
+  unsigned long countAt = 0; // when count changed
+  unsigned int countPrinted = 0; // last count printed
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  buttonState4 = digitalRead(buttonPin4);
+  //replace key Z with your desired key
+  if (pushButton.update()){
+    if (pushButton.fallingEdge()) {
+        count = count +1;
+        Keyboard.set_key1(KEY_8);
+        Keyboard.set_key2(KEYPAD_ASTERIX);
+        Keyboard.send_now();
+        countAt = millis();
+    }
+   } else {
+    if (count != countPrinted) {
+      unsigned long nowMillis = millis();
+      if (nowMillis - countAt > 100) {
+        Serial.print("count: ");
+        Serial.println(count);
+        countPrinted = count;
+      }
+    }
+  }  
+  if (buttonState == HIGH){
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+  if (buttonState2 == LOW){
+    Keyboard.set_key2(KEY_X);
+    Keyboard.send_now();
+  }
+  if (buttonState2 == HIGH){
+    Keyboard.set_key2(0);
+    Keyboard.send_now();
+  }
+  if (buttonState3 == LOW){
+    Keyboard.set_key3(KEY_C);
+    Keyboard.send_now();
+  }
+  if (buttonState3 == HIGH){
+    Keyboard.set_key3(0);
+    Keyboard.send_now();
+  }
+  if (buttonState4 == LOW){
+    Keyboard.set_key4(KEY_A);
+    Keyboard.send_now();
+  }
+  if (buttonState4 == HIGH){
+    Keyboard.set_key4(0);
+    Keyboard.send_now();
+  }
+  delay(2000);
+}
