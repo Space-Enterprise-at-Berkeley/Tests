5ff83e0dc2c7bb37d927e4a69635c17404a0f666
Pratyush Das <42460747+das2002@users.noreply.github.com>
2021-04-11 14:50:52 -0700

    Update ben_button_test_1.ino

--- teensy_switchboard_controller/ben_button_test_1.ino
+++ teensy_switchboard_controller/ben_button_test_1.ino
@@ -35,7 +35,11 @@ void loop(){
         Keyboard.set_key2(KEYPAD_ASTERIX);
         Keyboard.send_now();
         countAt = millis();
-    }
+    } else if (pushButton.risingEdge()) {
+         Keyboard.set_key1(0);
+         Keyboard.set_key2(0);
+         Keyboard.send_now();
+      }
    } else {
     if (count != countPrinted) {
       unsigned long nowMillis = millis();
